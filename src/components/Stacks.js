e6a65ec8dda115c53f95fc3d5dcdf015e993410a
KokuKUSIAKU <kusiaku.k@gmail.com>
2018-01-26 15:10:54 +0000

    svg url fixed

--- src/components/Stacks.js
+++ src/components/Stacks.js
@@ -1,15 +1,15 @@
 import React from "react";
-
+const PATH_ROOT = "https://cdn.rawgit.com/KokuKUSIAKU/HOME/master";
 const STACK_REGISTRY = [
-  { name: "HTML5", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/javascript-plain.svg", alt:"html language icon"}}, 
-  { name: "CSS3", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/css3-plain.svg", alt:"CSS language icon"}}, 
-  { name: "Bootstrap", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/bootstrap-plain.svg", alt:"Bootstrap framework icon"}},
-  { name: "JavaScript", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/javascript-plain.svg", alt:"JavaScript language icon"}}, 
-  { name: "React", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/react-original.svg", alt:"React framework icon"}}, 
-  { name: "Redux", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/javascript-plain.svg", alt:"Redux framework icon"}}, 
-  { name: "Jasmine", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/jasmine-plain.svg", alt:"Jasmine testing framework icon"}}, 
-  { name: "Webpack", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/webpack-plain.svg", alt:"Webpack tool icon"}}, 
-  { name: "Git", icon:{url:"https://github.com/KokuKUSIAKU/HOME/blob/master/images/git-plain.svg", alt:"Git tool icon"}}, 
+  { name: "HTML5", icon:{url: PATH_ROOT + "/images/javascript-plain.svg", alt:"html language icon"}}, 
+  { name: "CSS3", icon:{url:PATH_ROOT +"/images/css3-plain.svg", alt:"CSS language icon"}}, 
+  { name: "Bootstrap", icon:{url:PATH_ROOT +"/images/bootstrap-plain.svg?", alt:"Bootstrap framework icon"}},
+  { name: "JavaScript", icon:{url:PATH_ROOT +"/images/javascript-plain.svg?sanitize=true", alt:"JavaScript language icon"}}, 
+  { name: "React", icon:{url:PATH_ROOT +"/images/react-original.svg", alt:"React framework icon"}}, 
+  { name: "Redux", icon:{url:PATH_ROOT +"/images/javascript-plain.svg", alt:"Redux framework icon"}}, 
+  { name: "Jasmine", icon:{url:PATH_ROOT +"/images/jasmine-plain.svg", alt:"Jasmine testing framework icon"}}, 
+  { name: "Webpack", icon:{url:PATH_ROOT +"/images/webpack-plain.svg", alt:"Webpack tool icon"}}, 
+  { name: "Git", icon:{url:PATH_ROOT +"/images/git-plain.svg", alt:"Git tool icon"}}, 
 ];
 
 const StackItem = ({ name, icon }) => (
